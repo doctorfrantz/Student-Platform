27e97aa476c42b0e49596673d1d81adbd7daa807
Alexis Frantz <loyallombardi@gmail.com>
2023-03-03 10:53:48 +0100

    Initial Spring Setup

---
+++ src/main/java/com/alexisfrantz/studentplatform/StudentplatformApplication.java
@@ -0,0 +1,13 @@
+package com.alexisfrantz.studentplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexisfrantz/studentplatform/StudentplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexisfrantz.studentplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
